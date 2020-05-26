ac86d030f91798433c12f2490dcf909c743bbaff
latifnizamov <nizomovlatif@gmail.com>
2020-05-26 13:53:29 +0500

    Callback функции

--- lesson 3 /preview/app.js
+++ lesson 3 /preview/app.js
@@ -13,32 +13,50 @@
 // let calc = function (a, b){  
 //     return (a + b);
 // }
-let calc = (a,b) => a+b  // стрелочная функция
+// let calc = (a,b) => a+b  // стрелочная функция
 
-console.log(calc(3, 4));
+// console.log(calc(3, 4));
 
-console.log(calc(8, 4));
+// console.log(calc(8, 4));
 
-function retVar (){
-    let num = 50;
-    return num;
+// function retVar (){
+//     let num = 50;
+//     return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+// let str = 'tEst';
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let twelve = '12.2px';
+// // console.log(Math.round(twelve));
+// console.log(parseInt(twelve));
+// console.log(parseFloat(twelve));
+
+
+function first() {
+    //Что-то делаем 
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+function second() {
+    console.log(2);
+}
+first();
+second();
+
+function learnJS(lang, callback){
+    console.log('Я учу ' + lang);
+    callback();
 }
 
-let anotherNum = retVar();
-console.log(anotherNum);
-
-let str = 'tEst';
-console.log(str.length);
-
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-let twelve = '12.2px';
-// console.log(Math.round(twelve));
-console.log(parseInt(twelve));
-console.log(parseFloat(twelve));
-
-
-
-
- 
+function done() {
+    console.log('Я прошел 3й урок');
+}
+ learnJS('JS', done)
