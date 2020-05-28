6ca9b0eecfa482a0b478399b47d273a21cc3446b
latifnizamov <nizomovlatif@gmail.com>
2020-05-28 17:26:35 +0500

    Обьетно Ориентированное прогаммирование

--- lesson 4/preview/app.js
+++ lesson 4/preview/app.js
@@ -60,3 +60,16 @@ console.log(Object.keys(options).length);
         return a-b;
     }
     console.log(arr);
+
+let soldier = {
+    health: 400,
+    armor: 100
+};
+let john = {
+    health: 100
+};
+
+john.__proto__=soldier;
+
+console.log(john);
+console.log(john.armor);
