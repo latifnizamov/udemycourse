e6e647cc8e45362372b8ab35241db9c17f8e03e1
latifnizamov <nizomovlatif@gmail.com>
2020-05-29 13:36:10 +0500

    Задание 5 из видео

---
+++ lesson 4/app.js
@@ -0,0 +1,71 @@
+let money, time;
+
+function start (){
+    money = +prompt('Ваш бюджет на месяц?', ' '),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+    while (isNaN(money) || money == '' || money == null){
+        money = +prompt('Ваш бюджет на месяц?', ' ');
+    }
+}
+start();
+
+let appData = {
+    budjet: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: true,
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++){
+            let a = prompt('Ввдеите обязательную статью расходов в этом месяце', ''),
+                b = prompt('Во сколько обойдется ?', '');
+            if ((typeof (a) )=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+                && a != '' && b != '' && a.length < 50) {
+                console.log('done');
+                appData.expenses[a] = b;
+            } else {
+                i = i - 1;
+            }
+        }   
+    }, 
+    detectDayBudjet: function () {
+        appData.moneyPerDay = (appData.budjet / 30).toFixed();
+        alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+    },
+    detectLevel: function () {
+        if (appData.moneyPerDay < 100){
+            console.log('Минимальный уровень достатка');
+        }else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+            console.log('Средний уровень достатка');
+        } else if (appData.moneyPerDay > 2000){
+            console.log('Высокий уровень достатка');
+        } else {
+            console.log('Произошла ошибка')  
+        }
+    },
+    checkSavings: function () {
+        if(appData.savings == true){
+            let save = +prompt('Какова сумма накоплений?'),
+                percent = +prompt('Под какой процент?');
+    
+            appData.monthIncome = save/100/12*percent;
+            alert('Доход в месяц с вашего депозита: ' + (appData.monthIncome).toFixed());
+    
+        }  
+    },
+    chooseOptExpenses: function() {
+        for(let i = 0; i < 4; i++){
+            let opt = +prompt('Статья неоязательных расходов?');
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt('Что принесет дополнительный доход?(Перечислите через запятую)' , '');
+        appData.income = items.split(', ');
+        appData.income.push(prompt('Может что-то еще?'));
+        appData.income.sort();
+    }
+};
+
