2588e238cb16c6b4b6af1a3d4612a7ab5be56001
latifnizamov <nizomovlatif@gmail.com>
2020-06-04 11:58:15 +0500

    Домашнее задание 5.2 и поменял название папок

---
+++ lesson 5 /homework 2 /js/main.js
@@ -0,0 +1,63 @@
+let start = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value'),
+    yearValue = document.getElementsByClassName('year-value'),
+    monthValue = document.getElementsByClassName('month-value'),
+    dayValue = document.getElementsByClassName('day-value'),
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesItemBtn = document.getElementsByClassName('expenses-item-btn'),
+    optionalExpensesBtn = document.getElementsByClassName('optionalexpenses-btn'),
+    countBudgetBtn = document.getElementsByClassName('count-budget-btn'),
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    savings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValueEl = document.querySelector('.year-value'),
+    monthValueEl = document.querySelector('.month-value'),
+    dayValueEl = document.querySelector('.day-value');
+
+
+
+    
+
+
+
+
+console.log(start);
+console.log(budgetValue);
+console.log(daybudgetValue);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalExpensesValue);
+console.log(incomeValue);
+console.log(monthSavingsValue);
+console.log(yearSavingsValue);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
+console.log(expensesItem);
+console.log(expensesItemBtn);
+console.log(optionalExpensesBtn);
+console.log(countBudgetBtn);
+console.log(optionalExpensesItem);
+console.log(chooseIncome);
+console.log(savings);
+console.log(chooseSum);
+console.log(choosePercent);
+console.log(yearValueEl);
+console.log(monthValueEl);
+console.log(dayValueEl);
+
+
+
+
+
+
+
