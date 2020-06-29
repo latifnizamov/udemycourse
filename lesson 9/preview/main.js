179a7ecd4aedcca1349f9c9ba80d4c4983041766
latifnizamov <nizomovlatif@gmail.com>
2020-06-29 10:36:16 +0500

    Конструкторы и классы. Контекст вызова(This)

---
+++ lesson 9/preview/main.js
@@ -0,0 +1,65 @@
+
+
+// function User (name, id ) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     };
+// }
+// User.prototype.exit = function (name) {
+//     console.log('Пользователь ' + this.name + ' ушел');
+// }
+
+// let ivan = new User ('Ivan', 25),
+//     alex = new User ('Alex', 20);
+
+// console.log(ivan);
+// console.log(alex);
+
+// ivan.exit();
+
+
+// 'use strict';
+
+// function showThis(a, b) {
+//     console.log(this);  
+//     function sum(){
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// } 
+// showThis(4, 5);
+// showThis(5, 5);
+
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//         function shout(){
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis(){
+        console.log(this);
+    }
+    showThis();
+})
+
+//   
+
+// 1) Просто вызов функции - window/undefined 
+// 2) Метод обьекта - this = обьект 
+// 3) Конструтор (new) - this = новый созданный обьект
+// 4) Указание конткретного контекста - call, apply, bind
